455f1da7190f31920ef69aa9dc10541e52f5c66d
Seda <87141568+SedaUsta@users.noreply.github.com>
2024-04-28 00:02:38 +0300

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,4 +1,6 @@
+
 import javafx.scene.shape.Rectangle;
+import javafx.animation.*;
 
 
 public class Car {
@@ -6,6 +8,8 @@ public class Car {
 	private Rectangle rectangle;
 	private String state;
 	private int crushedTime = 0;
+	private PathTransition pt;
+	private double rotation;
 	
 	Car(){
 		
@@ -36,6 +40,32 @@ public class Car {
 	public String getState() {
 		return this.state;
 	}
+
+	public int getCrushedTime() {
+		return crushedTime;
+	}
+
+	public void setCrushedTime(int crushedTime) {
+		this.crushedTime = crushedTime;
+	}
+
+	public PathTransition getPt() {
+		return pt;
+	}
+
+	public void setPt(PathTransition pt) {
+		this.pt = pt;
+	}
+
+	public double getRotation() {
+		return rotation;
+	}
+
+	public void setRotation(double rotation) {
+		this.rotation = rotation;
+	}
+	
+	
 	
 	
 	
