4301e0ec7f6bcfdd1c3997d64f54ea27f8236280
ebrarceka <151394851+ebrarceka@users.noreply.github.com>
2024-05-10 16:48:59 +0300

    Update Marker.java

--- Marker.java
+++ Marker.java
@@ -1,75 +1,25 @@
 
-import javafx.beans.property.DoubleProperty;
-import javafx.scene.layout.Pane;
+
+
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
+
 
 public class Marker {
 private double centerX;
 private double centerY;
 private Circle circle;
 
-private boolean selected = false;
-private Line connectingLine;
-private final DoubleProperty mouseXProperty;
-private final DoubleProperty mouseYProperty;
-	
 	
 	Marker(double centerX, double centerY ){
 	this.centerX=centerX;
 	this.centerY=centerY;
-	mouseXProperty=null;
-	mouseYProperty=null;
+
 	
 		
 	circle = new Circle(centerX, centerY,3.7);
 		circle.setFill(Color.DARKGRAY);
 	}
-
-	 
-
-	    public Marker(DoubleProperty mouseXProperty, DoubleProperty mouseYProperty) {
-	        this.mouseXProperty = mouseXProperty;
-	        this.mouseYProperty = mouseYProperty;
-	    }
-
-	    // Method to handle mouse click on the marker
-	    public void handleMouseClick(Pane pane) {
-	        if (!selected) {
-	            select();
-	            if (connectingLine == null) {
-	                connectingLine = new Line();
-	                pane.getChildren().add(connectingLine);
-	            }
-	            // Set the starting point of the line to the marker's center
-	            double startX = getCenterX() + getCircle().getRadius();
-	            double startY = getCenterY() + getCircle().getRadius();
-	            connectingLine.setStartX(startX);
-	            connectingLine.setStartY(startY);
-	            // Bind the end point of the line to the mouse movement
-	            connectingLine.endXProperty().bind(pane.getScene().widthProperty().subtract(pane.getScene().widthProperty().subtract(mouseXProperty)));
-	            connectingLine.endYProperty().bind(pane.getScene().heightProperty().subtract(pane.getScene().heightProperty().subtract(mouseYProperty)));
-	        } else {
-	            deselect();
-	            // Remove the line
-	            pane.getChildren().remove(connectingLine);
-	            connectingLine = null;
-	        }
-	    }
-
-	    // Method to select the marker
-	    private void select() {
-	        selected = true;
-	        // Change the appearance of the marker when selected
-	    }
-
-	    // Method to deselect the marker
-	    private void deselect() {
-	        selected = false;
-	        // Restore the appearance of the marker when deselected
-	    }
-
 
 
 
